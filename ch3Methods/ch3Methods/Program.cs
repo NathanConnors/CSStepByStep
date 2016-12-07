f7211d11bb75e0180512a27edb4ae6f68dc212ac
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-07 14:07:04 -0800

    Chapter 3 Complete

--- ch3Methods/ch3Methods/Program.cs
+++ ch3Methods/ch3Methods/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ch3Methods
 {
@@ -10,6 +6,34 @@ namespace ch3Methods
     {
         static void Main(string[] args)
         {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double fee = calculateFee(noOfDays : 5);
+            Console.WriteLine("Fee is {0}", fee);
+        }
+
+        private double calculateFee(double dailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("calculateFee using two optional parameters");
+            return dailyRate * noOfDays;
+        }
+
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("calculateFee using one optional parameter");
+            int defaultNoOfDays = 1;
+            return dailyRate * defaultNoOfDays;
+        }
+
+        private double calculateFee()
+        {
+            Console.WriteLine("calculateFee using hardcoded values");
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+            return defaultDailyRate * defaultNoOfDays;
         }
     }
 }
