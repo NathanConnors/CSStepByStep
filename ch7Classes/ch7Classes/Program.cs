41383b4ca41ba0d63022a4ca1d8478a247f4a864
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-07 14:54:20 -0800

    Chapter 7 Start

---
+++ ch7Classes/ch7Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Chapter 7 - Creating and managing classes and objects
+ * Page 169
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
