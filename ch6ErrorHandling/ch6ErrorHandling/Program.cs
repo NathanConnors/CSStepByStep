102a78e286df78f8a12a597cdbd1117e407e3ee7
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-07 14:45:38 -0800

    Chapter 6 Complete

--- ch6ErrorHandling/ch6ErrorHandling/Program.cs
+++ ch6ErrorHandling/ch6ErrorHandling/Program.cs
@@ -10,6 +10,26 @@ namespace ch6ErrorHandling
     {
         static void Main(string[] args)
         {
+            //try
+            //{
+            //    // statements causing exception
+            //}
+            //catch (ExceptionName e1)
+            //{
+            //    // error handling code
+            //}
+            //catch (ExceptionName e2)
+            //{
+            //    // error handling code
+            //}
+            //catch (ExceptionName eN)
+            //{
+            //    // error handling code
+            //}
+            //finally
+            //{
+            //    // statements to be executed
+            //}
         }
     }
 }
