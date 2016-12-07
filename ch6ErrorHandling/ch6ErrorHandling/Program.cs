33004c09102b1aae7fa3a75d71c46de8194714a9
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-07 14:08:50 -0800

    Chapter 6 Start

---
+++ ch6ErrorHandling/ch6ErrorHandling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch6ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
